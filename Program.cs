3caccbff43a540a73ae50606c447a09aa69ea65a
Положенцев Егор Васильевич <Egor.Polozhentsev@norbit.ru>
2022-09-18 18:23:30 +0300

    Release 1.0.0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinuxCG
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Машина: {1}{0}ОС: {2}{0}Пользователь: {3}{0}", Environment.NewLine, Environment.MachineName, Environment.OSVersion, Environment.UserName);
+		}
+	}
+}
